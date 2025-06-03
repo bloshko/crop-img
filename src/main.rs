8ad4fc7fb11763b060f2998dcab351b02efeed54
bloshko <mis.bloko@gmail.com>
2025-06-03 20:00:41 +0200

    improve arg behaviour

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,37 @@ use std::path::Path;
 #[command(version = "0.1")]
 #[command(about = "Crops imgs", long_about = None)]
 struct Args {
-    #[arg(long)]
+    #[arg(long = "src", short = 's', required = true)]
     src_path: String,
-    #[arg(long)]
+    #[arg(long = "dest", short = 'd', required = true)]
     dest_path: String,
-    #[arg(long)]
+    #[arg(long = "dest_format", default_value = "png")]
+    format: String,
+    #[arg(long = "dest_size", default_value_t = 64)]
     size: u32,
 }
 
-#[allow(dead_code)]
-fn crop_and_write_img(src_path: String, dest_path: String, size: u32) {
-    let img = image::open(src_path).unwrap();
+fn crop_and_write_img(src_path: &Path, dest_path: &Path, size: u32, img_format: ImageFormat) {
+    let img = image::open(src_path).expect("read src_path");
     let mid_width = img.width().div_ceil(2) - size;
     let mid_height = img.height().div_ceil(2) - size;
 
     let cropped_img = img.crop_imm(mid_width, mid_height, size, size);
 
-    println!("{mid_width} {mid_height}");
-
     cropped_img
-        .save_with_format(dest_path, image::ImageFormat::Png)
-        .unwrap();
+        .save_with_format(dest_path, img_format)
+        .expect("save cropped image");
 }
 
 fn main() {
     let args = Args::parse();
+
+    assert!(
+        ImageFormat::from_extension(&args.format).is_some(),
+        "{} image format is not supported",
+        &args.format
+    );
+    let format = args.format;
 
     let src_path = Path::new(&args.src_path);
     assert!(
@@ -50,13 +56,28 @@ fn main() {
 
     let src_dir = fs::read_dir(&args.src_path).expect("reading src_dir succeeds");
 
-    let img_files: Vec<DirEntry> = src_dir
+    let img_dir_entries = src_dir
         .filter_map(Result::ok)
         .filter(|entry| ImageFormat::from_path(entry.path().as_path()).is_ok())
-        .collect();
-
+        .collect::<Vec<DirEntry>>();
     assert!(
-        !img_files.is_empty(),
+        !img_dir_entries.is_empty(),
         "src_path folder does not have image files"
     );
+
+    for entry in img_dir_entries.iter() {
+        let src_path = entry.path();
+        let file_name = entry.file_name();
+
+        let mut dest_path_buffer = dest_path.to_path_buf();
+        dest_path_buffer.push(file_name);
+        dest_path_buffer.set_extension(&format);
+
+        crop_and_write_img(
+            src_path.as_path(),
+            dest_path_buffer.as_path(),
+            args.size,
+            ImageFormat::from_extension(&format).unwrap(),
+        );
+    }
 }
